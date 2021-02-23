930ca25c61abd84936590e173b7001073996f330
Ermygut <ermygut@gmail.com>
2021-02-23 11:46:36 +0200

    add smooth scroll with arrow

--- src/js/script.js
+++ src/js/script.js
@@ -94,4 +94,19 @@ $(document).ready(function(){
 				}); 
 				return false;
 			});
+
+			//smooth scroll and page up
+			$(window).scroll(function(){
+				if ($(this).scrollTop() > 1000) {
+					$('.pageup').fadeIn('slow');
+				} else {
+					$('.pageup').fadeOut('slow');
+				}
+			});
+
+			$("a[href^='#']").click(function(){
+				const _href = $(this).attr("href");
+				$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+				return false;
+			});
 	 });
